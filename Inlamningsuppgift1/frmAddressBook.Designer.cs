4968d3e0e17de13119f57c628eb3ed0ec62c19d3
annasandberg <Anna.Kallman@yh.nackademin.se>
2016-10-16 17:26:33 +0200

    Add project files.

---
+++ Inlamningsuppgift1/frmAddressBook.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Adressbok
+{
+    partial class frmAddressBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNamn = new System.Windows.Forms.Label();
+            this.txtNamn = new System.Windows.Forms.TextBox();
+            this.txtGatuadress = new System.Windows.Forms.TextBox();
+            this.lblPostnr = new System.Windows.Forms.Label();
+            this.txtPostnr = new System.Windows.Forms.TextBox();
+            this.lblGatuadress = new System.Windows.Forms.Label();
+            this.txtTelefonnr = new System.Windows.Forms.TextBox();
+            this.lblTelefonnummer = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPostadress = new System.Windows.Forms.Label();
+            this.txtPostadress = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstAdresser = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSok = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNamn
+            // 
+            this.lblNamn.AutoSize = true;
+            this.lblNamn.Location = new System.Drawing.Point(20, 41);
+            this.lblNamn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNamn.Name = "lblNamn";
+            this.lblNamn.Size = new System.Drawing.Size(45, 17);
+            this.lblNamn.TabIndex = 0;
+            this.lblNamn.Text = "Namn";
+            // 
+            // txtNamn
+            // 
+            this.txtNamn.Location = new System.Drawing.Point(147, 37);
+            this.txtNamn.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNamn.Name = "txtNamn";
+            this.txtNamn.Size = new System.Drawing.Size(216, 22);
+            this.txtNamn.TabIndex = 1;
+            // 
+            // txtGatuadress
+            // 
+            this.txtGatuadress.Location = new System.Drawing.Point(147, 74);
+            this.txtGatuadress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGatuadress.Name = "txtGatuadress";
+            this.txtGatuadress.Size = new System.Drawing.Size(216, 22);
+            this.txtGatuadress.TabIndex = 2;
+            // 
+            // lblPostnr
+            // 
+            this.lblPostnr.AutoSize = true;
+            this.lblPostnr.Location = new System.Drawing.Point(20, 114);
+            this.lblPostnr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPostnr.Name = "lblPostnr";
+            this.lblPostnr.Size = new System.Drawing.Size(87, 17);
+            this.lblPostnr.TabIndex = 2;
+            this.lblPostnr.Text = "Postnummer";
+            // 
+            // txtPostnr
+            // 
+            this.txtPostnr.Location = new System.Drawing.Point(147, 111);
+            this.txtPostnr.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPostnr.Name = "txtPostnr";
+            this.txtPostnr.Size = new System.Drawing.Size(216, 22);
+            this.txtPostnr.TabIndex = 3;
+            // 
+            // lblGatuadress
+            // 
+            this.lblGatuadress.AutoSize = true;
+            this.lblGatuadress.Location = new System.Drawing.Point(20, 78);
+            this.lblGatuadress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGatuadress.Name = "lblGatuadress";
+            this.lblGatuadress.Size = new System.Drawing.Size(82, 17);
+            this.lblGatuadress.TabIndex = 4;
+            this.lblGatuadress.Text = "Gatuadress";
+            // 
+            // txtTelefonnr
+            // 
+            this.txtTelefonnr.Location = new System.Drawing.Point(147, 185);
+            this.txtTelefonnr.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTelefonnr.Name = "txtTelefonnr";
+            this.txtTelefonnr.Size = new System.Drawing.Size(216, 22);
+            this.txtTelefonnr.TabIndex = 5;
+            // 
+            // lblTelefonnummer
+            // 
+            this.lblTelefonnummer.AutoSize = true;
+            this.lblTelefonnummer.Location = new System.Drawing.Point(20, 188);
+            this.lblTelefonnummer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTelefonnummer.Name = "lblTelefonnummer";
+            this.lblTelefonnummer.Size = new System.Drawing.Size(107, 17);
+            this.lblTelefonnummer.TabIndex = 6;
+            this.lblTelefonnummer.Text = "Telefonnummer";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(147, 222);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(216, 22);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // lblPostadress
+            // 
+            this.lblPostadress.AutoSize = true;
+            this.lblPostadress.Location = new System.Drawing.Point(20, 151);
+            this.lblPostadress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPostadress.Name = "lblPostadress";
+            this.lblPostadress.Size = new System.Drawing.Size(79, 17);
+            this.lblPostadress.TabIndex = 8;
+            this.lblPostadress.Text = "Postadress";
+            // 
+            // txtPostadress
+            // 
+            this.txtPostadress.Location = new System.Drawing.Point(147, 148);
+            this.txtPostadress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPostadress.Name = "txtPostadress";
+            this.txtPostadress.Size = new System.Drawing.Size(216, 22);
+            this.txtPostadress.TabIndex = 4;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(20, 225);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 10;
+            this.lblEmail.Text = "Email";
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Location = new System.Drawing.Point(301, 330);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Skapa";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(301, 12);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 28);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "Sök";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstAdresser
+            // 
+            this.lstAdresser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstAdresser.FormattingEnabled = true;
+            this.lstAdresser.ItemHeight = 16;
+            this.lstAdresser.Location = new System.Drawing.Point(417, 1);
+            this.lstAdresser.Margin = new System.Windows.Forms.Padding(4);
+            this.lstAdresser.Name = "lstAdresser";
+            this.lstAdresser.Size = new System.Drawing.Size(451, 372);
+            this.lstAdresser.TabIndex = 14;
+            this.lstAdresser.SelectedIndexChanged += new System.EventHandler(this.lstAdresser_SelectedIndexChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(20, 330);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Ta bort";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPostadress);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.lblPostadress);
+            this.groupBox1.Controls.Add(this.txtTelefonnr);
+            this.groupBox1.Controls.Add(this.lblTelefonnummer);
+            this.groupBox1.Controls.Add(this.txtPostnr);
+            this.groupBox1.Controls.Add(this.lblGatuadress);
+            this.groupBox1.Controls.Add(this.txtGatuadress);
+            this.groupBox1.Controls.Add(this.lblPostnr);
+            this.groupBox1.Controls.Add(this.txtNamn);
+            this.groupBox1.Controls.Add(this.lblNamn);
+            this.groupBox1.Location = new System.Drawing.Point(20, 57);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(381, 262);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Skapa kontakt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(7, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "*";
+            // 
+            // txtSok
+            // 
+            this.txtSok.Location = new System.Drawing.Point(20, 15);
+            this.txtSok.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSok.Name = "txtSok";
+            this.txtSok.Size = new System.Drawing.Size(272, 22);
+            this.txtSok.TabIndex = 17;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(128, 330);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(165, 28);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Uppdatera kontakt";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // frmAddressBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 369);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtSok);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lstAdresser);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmAddressBook";
+            this.Text = "Adressbok";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNamn;
+        private System.Windows.Forms.TextBox txtNamn;
+        private System.Windows.Forms.TextBox txtGatuadress;
+        private System.Windows.Forms.Label lblPostnr;
+        private System.Windows.Forms.TextBox txtPostnr;
+        private System.Windows.Forms.Label lblGatuadress;
+        private System.Windows.Forms.TextBox txtTelefonnr;
+        private System.Windows.Forms.Label lblTelefonnummer;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPostadress;
+        private System.Windows.Forms.TextBox txtPostadress;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lstAdresser;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSok;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Inlamningsuppgift1/frmAddressBook.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Mail;
+
+//Uppgiften handlar om att skapa en adressbok.All information skall sparas i en textfil.Det
+//betyder att applikationen skall kunna läsa och skriva från denna textfil.
+//Krav för att uppgiften skall bli godkänd:
+//- Följande information skall kunna registreras och sparas till textfilen, namn, gatudress,
+//postnummer, postort, telefon och epost
+//- Det skall gå att spara en ny adress i adressboken men också uppdatera en adress som
+//redan finns.
+//- Det skall gå att ta bort en adress och den skall då försvinna från adressboken.
+//- Det skall gå att söka på en eller flera adresser och sökresultatet skall visas i en lista.
+//Från listan skall det gå att klicka på en rad och få upp all information om den adressen.
+//- Sökningen skall fungera som ett urval där det minst skall gå att göra urval på namn
+//och postort.
+//Koden skall fungera och applikationen skall gå att köra utan fel.
+
+//ev. gör objektorienterat
+
+namespace Adressbok
+{
+    public partial class frmAddressBook : Form
+    {
+        string path = "C:\\Users\\AnnaDaniel\\Documents\\Nackademin\\Adressbok.txt";
+        List<string> lines = new List<string>();
+
+        public frmAddressBook()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the ContactInfo.
+        /// </summary>
+        /// <returns></returns>
+        public string ContactInfo()
+        {
+            string name = txtNamn.Text;
+            string street = txtGatuadress.Text;
+            string zipCode = txtPostnr.Text;
+            string city = txtPostadress.Text;
+            string phoneNr = txtTelefonnr.Text;
+            string email = txtEmail.Text;
+            return name+ ", "+ street+", "+zipCode+", "+city+", "+phoneNr+", "+email;
+        }
+
+        /// <summary>
+        /// Empties all textboxes in groupbox.
+        /// </summary>
+        public void ClearTextboxes()
+        {
+            txtNamn.Clear();
+            txtGatuadress.Clear();
+            txtPostadress.Clear();
+            txtPostnr.Clear();
+            txtTelefonnr.Clear();
+            txtEmail.Clear();
+        }
+
+        /// <summary>
+        /// Validates email address.
+        /// </summary>
+        /// <param name="emailadress"></param>
+        /// <returns></returns>
+        private bool EmailIsValid(string emailadress)
+        {
+            int emailLength = emailadress.Length;
+
+            if (emailadress.Contains("@") && !emailadress.Contains(" "))
+            {
+                if ((emailadress.LastIndexOf('.') == (emailLength - 4)) ||
+                    (emailadress.LastIndexOf('.') == (emailLength - 3)))
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Adds what info is missing to string.
+        /// </summary>
+        /// <returns></returns>
+        private string MissingInfoToString()
+        {
+            string message = "";
+            if (string.IsNullOrEmpty(txtNamn.Text))
+            {
+                message += "Namn\n";
+            }
+            if (string.IsNullOrEmpty(txtGatuadress.Text))
+            {
+                message += "Gatuadress\n";
+            }
+            if (string.IsNullOrEmpty(txtPostnr.Text))
+            {
+                message += "Postnummer\n";
+            }
+            if (string.IsNullOrEmpty(txtPostadress.Text))
+            {
+                message += "Postadress\n";
+            }
+            if (string.IsNullOrEmpty(txtTelefonnr.Text))
+            {
+                message += "Telefonnummer\n";
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// Checks if user has entered name and address.
+        /// </summary>
+        /// <returns></returns>
+        private bool NameAddressIsGiven()
+        {
+            if (string.IsNullOrEmpty(MissingInfoToString()))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Writes contact to text file.
+        /// </summary>
+        private void WriteToFile()
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(ContactInfo());
+            sw.Close();
+            ClearTextboxes();
+        }
+
+        private bool EmailExists(string email)
+        {
+            foreach (var line in lines)
+            {
+                if (line.Contains(txtEmail.Text))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Adds contact to text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (EmailIsValid(txtEmail.Text))
+            {
+                if (!EmailExists(txtEmail.Text))
+                {
+                    if (NameAddressIsGiven())
+                    {
+                        WriteToFile();
+                    }
+                    else
+                    {
+                        DialogResult dlg = MessageBox.Show("Följande fält saknas:\n" + MissingInfoToString() + "Vill du spara ändå?",
+                            "Fält saknas", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (dlg == DialogResult.Yes)
+                        {
+                            WriteToFile();
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Emailadressen finns redan, det går inte att spara");
+            }
+            else
+            {
+                MessageBox.Show("Du har angett en felaktig mailadress.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        /// <summary>
+        /// Creates a List of all addresses.
+        /// </summary>
+        public void AddressList()
+        {
+            var fileCheckOK = File.Exists(path) && new FileInfo(path).Length > 0;
+
+            if(fileCheckOK)
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+         }
+
+        /// <summary>
+        /// Clears listbox.
+        /// </summary>
+        public void EmptyListBox()
+        {
+            lstAdresser.DataSource = null;
+            lstAdresser.Items.Clear();
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lines.Clear();
+            AddressList();
+            lstAdresser.DataSource = SearchAddressBook();
+        }
+
+        private List<string> SearchAddressBook()
+        {
+            List<string> searchList = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line.ToLower().Contains(txtSok.Text.ToLower()))
+                {
+                    searchList.Add(line);
+                }
+            }
+
+            EmptyListBox();
+
+            return searchList;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstAdresser.SelectedIndex >= 0)
+            {
+                var selectedPerson = lstAdresser.SelectedItem.ToString();
+                lines.Remove(selectedPerson);
+                File.WriteAllLines(path, lines);
+                lstAdresser.DataSource = null;
+                lstAdresser.DataSource = SearchAddressBook();
+                ClearTextboxes();
+            }
+        }
+
+        private void lstAdresser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstAdresser.SelectedItem != null)
+            {
+                string rad = lstAdresser.SelectedItem.ToString();
+                
+                var kontaktinfo = rad.Split(',');
+                txtNamn.Text = kontaktinfo[0].Trim();
+                txtGatuadress.Text = kontaktinfo[1].Trim();
+                txtPostnr.Text = kontaktinfo[2].Trim();
+                txtPostadress.Text = kontaktinfo[3].Trim();
+                txtTelefonnr.Text = kontaktinfo[4].Trim();
+                txtEmail.Text = kontaktinfo[5].Trim();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (lstAdresser.SelectedValue == null)
+                return;
+
+            var selectedValue = lstAdresser.SelectedValue.ToString();
+            var newLines = lines.Select(textValue => textValue.Equals(selectedValue) ?
+                            textValue.Replace(selectedValue, ContactInfo()) : textValue);
+            
+            File.WriteAllLines(path, newLines);
+
+            lines.Clear();
+            AddressList();
+            EmptyListBox();
+            lstAdresser.DataSource = lines;
+            ClearTextboxes();
+
+        }
+    }
+}
